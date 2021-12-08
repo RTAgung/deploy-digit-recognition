254e3e8fea12882e5e4da32bbbd867313dac2af5
Rama Tri Agung <ramatriagung91@gmail.com>
2021-12-08 22:27:15 +0700

    add final code app.py

--- app.py
+++ app.py
@@ -11,7 +11,18 @@ scaler = joblib.load('std_scaler.bin')
 class_dict = {0: 'Nol', 1: 'Satu', 2: 'Dua', 3: 'Tiga', 4: 'Empat', 5: 'Lima', 6: 'Enam', 7: 'Tujuh', 8: 'Delapan', 9: 'Sembilan'}
 
 def predict_label(img_path):
-    return class_dict[0]
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = cv2.resize(img, (22, 22), interpolation = cv2.INTER_AREA)
+    
+    img = img.flatten()
+    img = img.reshape(1,-1)
+    img = scaler.transform(img)
+
+    classes = model.predict(img)
+    print("Digit Hasil Prediksi :", classes[0])
+    
+    return class_dict[classes[0]]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
